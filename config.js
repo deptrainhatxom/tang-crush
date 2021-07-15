2da0cc87c4c752ab6510629e6771a9febaaac635
deptrainhatxom <87462715+deptrainhatxom@users.noreply.github.com>
2021-07-15 18:32:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Hồng à!',
-    introDesc: `Trái đất vốn lạ thường\n
-    Mà sao em cứ đi nhầm đường\n
-    Lạc vào tim anh lẻ loi\n
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Yêu một người không khó
+Cậu có muốn làm điều đó cùng mình không?`,
     btnIntro: 'Ủa cái gì zậy',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Mình thích cậu đấy cậu thì sao 😊😊😊 ',
     btnYes: 'Em thích anh cũng lâu lắm rồi 😘',
     btnNo: 'Không, Mình chỉ xem cậu là bạn 😢',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ❤',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu mình ❤',
     btnReply: 'Gửi cho anh ❤❤❤',
     reply: 'Là do anh đẹp trai quá đó ❤❤❤',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Nhưng mình sợ ba mẹ bạn lắm',
-    btnAccept: 'Mình lo',
+    mess: 'Mình biết mà 🥰. Một người đẹp như mình 😘😘',
+    messDesc: 'Thế mình gọi cậy là em nhá 💖💖💖 ',
+    btnAccept: 'Em yêu anh',
     messLink: 'https://www.facebook.com/tandeptrainhatxom/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
