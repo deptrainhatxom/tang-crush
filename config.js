e42c0d9d21cff7c36a9a906dd34fda729ed19b90
deptrainhatxom <87462715+deptrainhatxom@users.noreply.github.com>
2021-07-15 16:55:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Ủa cái gì zậy',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Em thích anh cũng lâu lắm rồi 😘',
+    btnNo: 'Không, Mình chỉ xem cậu là bạn 😢',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ❤',
+    btnReply: 'Gửi cho anh ❤❤❤',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
